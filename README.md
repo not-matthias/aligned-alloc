fceeba7517e2e3fc9117c3a80340237f41a38774
Matthias <26800596+not-matthias@users.noreply.github.com>
2024-03-05 08:39:07 +0100

    chore: add code and doc

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # aligned-alloc
+
+## Example
+
+```rust
+let memory: Box<[u8], _> =
+            unsafe { Box::new_zeroed_slice_in(100, AlignedAlloc::<4096>).assume_init() };
+```
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 //! Based on this: https://stackoverflow.com/a/69544158
 
 #![feature(allocator_api)]
+#![feature(new_uninit)]
 
 extern crate alloc;
 
@@ -21,5 +22,18 @@ unsafe impl<const N: usize> Allocator for AlignedAlloc<N> {
 
     unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
         Global.deallocate(ptr, layout.align_to(N).unwrap())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_align_box() {
+        let memory: Box<[u8], _> =
+            unsafe { Box::new_zeroed_slice_in(100, AlignedAlloc::<4096>).assume_init() };
+
+        assert_eq!(memory.as_ptr() as usize % 4096, 0);
     }
 }
